0414590e924de73d25857122a2ecb170c843c94d
Devesh Kumar <devesh2027@gmail.com>
2021-09-20 16:31:00 +0530

    Added user writable & persisted store.

--- src/store/user.js
+++ src/store/user.js
@@ -3,7 +3,7 @@ import { writable } from "svelte/store";
 const user = writable(localStorage.getItem("note-it-down-user") || null);
 
 user.subscribe((newUserValue) =>
-	localStorage.setItem("note-it-down-user", newUserValue)
+	localStorage.setItem("note-it-down-user", JSON.stringify(newUserValue))
 );
 
 export default user;
