0ea5b7e6c3f38be76b79b2502a9ef57834723883
Devesh Kumar <devesh2027@gmail.com>
2021-09-20 17:04:55 +0530

    Setup editor.js and dependencies
    Todo: Setup + Placeholder for components

---
+++ src/components/Editor.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { onMount } from "svelte";
+
+	// Editor
+	import EditorJS from "@editorjs/editorjs";
+	import EditorHeader from "@editorjs/header";
+	import EditorSimpleImage from "@editorjs/simple-image";
+	import EditorTable from "@editorjs/table";
+	import EditorInlineCode from "@editorjs/inline-code";
+	import EditorCodeBlock from "@editorjs/code";
+	import EditorLink from "@editorjs/link";
+	import EditorList from "@editorjs/list";
+	import EditorWarning from "@editorjs/warning";
+	import EditorDelimiter from "@editorjs/delimiter";
+
+	let editor;
+
+	onMount(() => {
+		editor = new EditorJS({
+			id: "editorjs",
+			tools: {
+				header: { class: EditorHeader, shortcut: "CMD+SHIFT+H" },
+				image: EditorSimpleImage,
+				inlineCode: EditorInlineCode,
+				code: EditorCodeBlock,
+				table: EditorTable,
+				delimiter: EditorDelimiter,
+				warning: EditorWarning,
+				list: { class: EditorList, shortcut: "CMD+7" },
+				link: { class: EditorLink, shortcut: "CMD+K" },
+			},
+		});
+	});
+</script>
+
+<div class="editor-wrapper">
+	<div id="editorjs" />
+</div>
+
+<style>
+	#editorjs {
+		border: 0.1rem solid #efefef;
+	}
+</style>
