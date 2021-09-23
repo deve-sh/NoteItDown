1b55a1a05c619a476b3867fbee54604c92c2b491
Devesh Kumar <devesh2027@gmail.com>
2021-09-23 09:33:32 +0530

    Added service-worker file

---
+++ public/service-worker.js
@@ -0,0 +1,5 @@
+var cacheName = "noteitdown-pwa";
+var filesToCache = ["/", "/index.html"];
+self.addEventListener("install", function (e) {});
+/* Serve cached content when offline */
+self.addEventListener("fetch", function (e) {});
