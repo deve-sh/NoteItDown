61e27a0c08cc9c7a84b5465d7c1e973657d741ef
Devesh Kumar <devesh2027@gmail.com>
2021-09-26 19:47:34 +0530

    Added redirect-to from ProtectedRoute

--- frontend/src/Wrappers/ProtectedRoute.jsx
+++ frontend/src/Wrappers/ProtectedRoute.jsx
@@ -4,11 +4,21 @@ import useStore from "hooks/useStore";
 const ProtectedRoute = ({ component: Component, ...rest }) => {
 	const user = useStore((state) => state.user);
 
+	let pathToRedirectTo = rest?.computedMatch?.url ?? "/";
+	const queryParams = rest?.location?.search ?? "";
+
+	if (queryParams && queryParams.startsWith("?"))
+		pathToRedirectTo += encodeURIComponent(queryParams);
+
 	return (
 		<Route
 			{...rest}
 			render={(props) =>
-				user ? <Component {...props} /> : <Redirect to="/" />
+				user ? (
+					<Component {...props} />
+				) : (
+					<Redirect to={`/login?redirect=${pathToRedirectTo}`} />
+				)
 			}
 		/>
 	);
