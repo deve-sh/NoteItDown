da6e1778e592eb5d904b55f9f8c39c920483b5d6
Devesh Kumar <devesh2027@gmail.com>
2021-09-26 12:41:02 +0530

    Fixed db import issue

--- frontend/src/firebase/db.js
+++ frontend/src/firebase/db.js
@@ -1,9 +1,8 @@
-// File to house the global database ref of firebase.
-
+import mainFirebase from "firebase/app";
 import firebase from "./index";
 import "firebase/firestore";
 
 const db = firebase.firestore();
 
 export default db;
-export const firestore = firebase.firestore;
+export const firestore = mainFirebase.firestore;
