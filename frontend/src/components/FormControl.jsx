bd40486acad305746080d15367b8724c6c532dea
Devesh Kumar <devesh2027@gmail.com>
2021-09-26 22:02:31 +0530

    Added Form-Control component

---
+++ frontend/src/components/FormControl.jsx
@@ -0,0 +1,47 @@
+import {
+	FormControl as ChakraFormControl,
+	FormLabel,
+	FormErrorMessage,
+	FormHelperText,
+	Input,
+} from "@chakra-ui/react";
+
+const FormControl = ({
+	value,
+	id,
+	className,
+	label,
+	onChange,
+	placeholder,
+	type,
+	errorMessage,
+	required,
+	disabled,
+	name,
+	helperText,
+}) => (
+	<ChakraFormControl
+		id={id}
+		className={className}
+		isRequired={required}
+		isReadOnly={disabled}
+		name={name}
+	>
+		<FormLabel>{label}</FormLabel>
+		<Input
+			type={type}
+			onChange={onChange}
+			placeholder={placeholder}
+			name={name}
+			disabled={disabled}
+			value={value}
+		/>
+		{errorMessage ? (
+			<FormErrorMessage>{errorMessage}</FormErrorMessage>
+		) : (
+			<FormHelperText>{helperText}</FormHelperText>
+		)}
+	</ChakraFormControl>
+);
+
+export default FormControl;
