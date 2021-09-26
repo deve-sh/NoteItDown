8de5c0e756dd06539a5c19e90df7f9be010f0e60
Devesh Kumar <devesh2027@gmail.com>
2021-09-26 11:26:38 +0530

    Added dark mode indicator to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,12 +4,21 @@ import { persist } from "zustand/middleware";
 const store = create(
 	persist(
 		(set) => ({
+			// User Auth
 			user: null,
 			setUser: (user = null) => set((state) => ({ ...state, user })),
+			// Loader
 			isLoading: false,
 			loaderType: "loader",
 			setLoading: (isLoading = false, loaderType = "loader") =>
 				set((state) => ({ ...state, isLoading, loaderType })),
+			// Dark Mode
+			isDarkModeActive: false,
+			toggleDarkMode: () =>
+				set((state) => ({
+					...state,
+					isDarkModeActive: !state.isDarkModeActive,
+				})),
 		}),
 		{
 			name: "hiqr-storage",
