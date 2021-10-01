07fa313d22b03a37e51c47736fd31b25a7478e91
Devesh Kumar <devesh2027@gmail.com>
2021-10-01 23:49:13 +0530

    Added function to save document to workspace.

--- frontend/src/API/documents.js
+++ frontend/src/API/documents.js
@@ -1,4 +1,5 @@
-import db from "firebase/db";
+import auth from "firebase/authentication";
+import db, { firestore } from "firebase/db";
 
 export const getDocumentsFromWorkspace = async (
 	workspaceId,
@@ -17,6 +18,30 @@ export const getDocumentsFromWorkspace = async (
 					.get()
 			).docs.map((doc) => ({ id: doc.id, ...doc.data() }))
 		);
+	} catch (err) {
+		console.log(err);
+		return callback(err.message);
+	}
+};
+
+export const addDocumentToWorkspace = async (
+	documentId,
+	documentUpdates = {},
+	callback
+) => {
+	try {
+		delete documentUpdates.workspace;
+		delete documentUpdates.createdBy;
+		delete documentUpdates.createdAt;
+
+		const documentRef = db.collection("documents").doc(documentId);
+		await documentRef.update({
+			...documentUpdates,
+			updatedAt: firestore.FieldValue.serverTimestamp(),
+			lastUpdatedBy: auth.currentUser.uid,
+		});
+
+		return callback(null, (await documentRef.get()).data());
 	} catch (err) {
 		console.log(err);
 		return callback(err.message);
