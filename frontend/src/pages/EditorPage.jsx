ebe8af44931ce6dc9d26f5b2b9f057be1df27b16
Devesh Kumar <devesh2027@gmail.com>
2021-10-02 23:37:40 +0530

    Added functions to save and create new workspace documents

--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useRef } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 
 import Editor from "components/Editor";
 import ContentWrapper from "Wrappers/ContentWrapper";
+
+import { addDocumentToWorkspace, updateDocument } from "API/documents";
+import toasts from "helpers/toasts";
 
 import useFirestore from "hooks/useFirestore";
 import useStore from "hooks/useStore";
 
 const EditorPage = (props) => {
+	const history = useHistory();
 	const editor = useRef(null);
 
 	const user = useStore((state) => state.user);
@@ -28,6 +32,7 @@ const EditorPage = (props) => {
 		data: documentData,
 		error: documentError,
 		isLoading: documentLoading,
+		revalidate: revalidateDocumentData,
 	} = useFirestore(documentId ? `documents/${documentId}` : null);
 
 	useEffect(() => {
@@ -51,9 +56,33 @@ const EditorPage = (props) => {
 			time: new Date().getTime(),
 		};
 
+	const saveDocument = async () => {
+		const editorData = await getEditorContent();
+		if (mode === "new") {
+			addDocumentToWorkspace(
+				workspaceId,
+				{ editorData },
+				(err, documentCreated) => {
+					if (err) return toasts.generateError(err);
+					history.push(`/editor/document/${documentCreated.id}`); // Take the user to the dedicated document page.
+				}
+			);
+		} else {
+			const updates = { editorData };
+			updateDocument(documentId, updates, (err) => {
+				if (err) return toasts.generateError(err);
+				revalidateDocumentData();
+			});
+		}
+	};
+
 	return (
 		<ContentWrapper>
-			<Editor onReady={(editorInstance) => (editor.current = editorInstance)} />
+			<Editor
+				onReady={(editorInstance) => (editor.current = editorInstance)}
+				readOnly={mode !== "new"}
+				prefilledData={documentData?.editorData}
+			/>
 		</ContentWrapper>
 	);
 };
