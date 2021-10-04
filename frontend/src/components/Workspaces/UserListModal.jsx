cca4b8412c48705a4366d4827749591c7c763cdc
Devesh Kumar <devesh2027@gmail.com>
2021-10-04 09:07:39 +0530

    Added user email input for adding new user to workspace

--- frontend/src/components/Workspaces/UserListModal.jsx
+++ frontend/src/components/Workspaces/UserListModal.jsx
@@ -1,4 +1,4 @@
-import auth from "firebase/authentication";
+import { useState } from "react";
 import {
 	Modal,
 	ModalOverlay,
@@ -17,10 +17,16 @@ import {
 	Avatar,
 	Text,
 	IconButton,
+	HStack,
 } from "@chakra-ui/react";
 import { RiKeyFill as AdminIcon } from "react-icons/ri";
 import { MdClose } from "react-icons/md";
+import { FaUserPlus } from "react-icons/fa";
+
+import auth from "firebase/authentication";
+
 import NoneFound from "components/NoneFound";
+import FormControl from "components/FormControl";
 
 const UserListModal = ({
 	showOptions = false,
@@ -29,71 +35,99 @@ const UserListModal = ({
 	onClose,
 	isLoading = false,
 	onUserRemoveClick = () => null,
-}) => (
-	<Modal isOpen={isOpen} onClose={onClose} size="xl">
-		<ModalOverlay />
-		<ModalContent>
-			<ModalHeader>Workspace Users</ModalHeader>
-			<ModalCloseButton />
-			<ModalBody>
-				{isLoading ? (
-					<Container centerContent>
-						<Spinner
-							size="xl"
-							thickness="4px"
-							color="blue.600"
-							marginBottom="1rem"
-						/>
-						Loading User List
-					</Container>
-				) : userList && userList?.length ? (
-					<Table>
-						<Tbody>
-							{userList.map((user) => (
-								<Tr key={user.uid}>
-									<Td>
-										<Avatar name={user.displayName} src={user.photoURL} />
-									</Td>
-									<Td>
-										{user.displayName}
-										{(user.isAdmin || false) && (
-											<AdminIcon size="1.125rem" title="Workspace Admin" />
-										)}
-									</Td>
-									<Td>
-										<Text color="gray">{user.email}</Text>
-									</Td>
-									<Td>
-										{showOptions && user.uid !== auth.currentUser.uid ? (
-											<>
-												<IconButton
-													colorScheme="red"
-													variant="ghost"
-													onClick={() => onUserRemoveClick(user.uid)}
-												>
-													<MdClose />
-												</IconButton>
-											</>
-										) : (
-											""
-										)}
-									</Td>
-								</Tr>
-							))}
-						</Tbody>
-					</Table>
-				) : (
-					<NoneFound label="No Users In The Workspace" />
-				)}
-			</ModalBody>
+	addNewUser = () => null,
+}) => {
+	const [newUserEmail, setNewUserEmail] = useState("");
 
-			<ModalFooter>
-				<Button colorScheme="red" onClick={onClose}>
-					Close
-				</Button>
-			</ModalFooter>
-		</ModalContent>
-	</Modal>
-);
+	return (
+		<Modal isOpen={isOpen} onClose={onClose} size="xl">
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Workspace Users</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					{showOptions ? (
+						<HStack alignItems="center">
+							{/* Section to add new user to workspace */}
+							<FormControl
+								name="email"
+								onChange={(e) => {
+									e.persist();
+									setNewUserEmail(e.target.value);
+								}}
+								disabled={isLoading}
+								placeholder="New User Email"
+							/>
+							<IconButton
+								colorScheme="teal"
+								onClick={() => addNewUser(newUserEmail)}
+								disabled={isLoading}
+							>
+								<FaUserPlus size="1.25rem" />
+							</IconButton>
+						</HStack>
+					) : (
+						""
+					)}
+					{isLoading ? (
+						<Container centerContent>
+							<Spinner
+								size="xl"
+								thickness="4px"
+								color="blue.600"
+								marginBottom="1rem"
+							/>
+							Loading User List
+						</Container>
+					) : userList && userList?.length ? (
+						<Table>
+							<Tbody>
+								{userList.map((user) => (
+									<Tr key={user.uid}>
+										<Td>
+											<Avatar name={user.displayName} src={user.photoURL} />
+										</Td>
+										<Td>
+											{user.displayName}
+											{(user.isAdmin || false) && (
+												<AdminIcon size="1.125rem" title="Workspace Admin" />
+											)}
+										</Td>
+										<Td>
+											<Text color="gray">{user.email}</Text>
+										</Td>
+										<Td>
+											{showOptions && user.uid !== auth.currentUser.uid ? (
+												<>
+													<IconButton
+														colorScheme="red"
+														variant="ghost"
+														onClick={() => onUserRemoveClick(user.uid)}
+													>
+														<MdClose />
+													</IconButton>
+												</>
+											) : (
+												""
+											)}
+										</Td>
+									</Tr>
+								))}
+							</Tbody>
+						</Table>
+					) : (
+						<NoneFound label="No Users In The Workspace" />
+					)}
+				</ModalBody>
+
+				<ModalFooter>
+					<Button colorScheme="red" onClick={onClose}>
+						Close
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
+	);
+};
 
 export default UserListModal;
