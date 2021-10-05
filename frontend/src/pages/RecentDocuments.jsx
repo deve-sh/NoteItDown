2c2ac58b5d4fa857f56e2a3c2a8bceefee6cf6a3
Devesh Kumar <devesh2027@gmail.com>
2021-10-05 15:55:54 +0530

    Setup recent documents page

---
+++ frontend/src/pages/RecentDocuments.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import styled from "@emotion/styled";
+import { List, ListItem, Text } from "@chakra-ui/react";
+
+import { getRecentDocumentsFromWorkspaces } from "API/documents";
+import toasts from "helpers/toasts";
+import useStore from "hooks/useStore";
+
+import ContentWrapper from "Wrappers/ContentWrapper";
+import NoneFound from "components/NoneFound";
+
+const DocumentsListWrapper = styled(ContentWrapper)`
+	max-width: 850px;
+`;
+const DocumentLink = styled(Link)`
+	padding: calc(0.5 * var(--standard-spacing));
+	width: 100%;
+	display: block;
+	&:hover {
+		background: var(--backgroundgrey);
+	}
+`;
+
+const RecentDocuments = () => {
+	// Page to have all documents recently updated or created in workspaces user's subscribed to.
+	const user = useStore((state) => state.user);
+	const setLoading = useStore((store) => store.setLoading);
+
+	const [recentDocuments, setRecentDocuments] = useState([]);
+
+	useEffect(() => {
+		setLoading(true);
+		getRecentDocumentsFromWorkspaces(
+			user?.workspaces || [],
+			(err, documents) => {
+				setLoading(false);
+				if (err) return toasts.generateError(err);
+				console.log(documents);
+				return setRecentDocuments(documents);
+			}
+		);
+	}, [user]);
+
+	return (
+		<DocumentsListWrapper>
+			{recentDocuments?.length ? (
+				<List spacing="3" textAlign="left" minWidth="650px" marginTop="2.5rem">
+					{recentDocuments.map((doc) => (
+						<ListItem key={doc.id}>
+							<DocumentLink to={`/editor/document/${doc.id}`} target="_blank">
+								<Text fontWeight="500">
+									{doc.identifierEmoji?.emoji || "📄"} {doc.title}
+								</Text>
+							</DocumentLink>
+						</ListItem>
+					))}
+				</List>
+			) : (
+				<NoneFound label="No Documents Found Recently." />
+			)}
+		</DocumentsListWrapper>
+	);
+};
+
+export default RecentDocuments;
