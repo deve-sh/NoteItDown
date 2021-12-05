c5fc20114885aaeb63cd7d3bc41a9709ec7ec143
Devesh Kumar <devesh2027@gmail.com>
2021-12-05 16:49:09 +0530

    Removed rc-mentions for now since it doesn't serve a use

--- frontend/src/components/Document/CommentInputBlock.jsx
+++ frontend/src/components/Document/CommentInputBlock.jsx
@@ -10,29 +10,32 @@ const CommentInputBlock = ({
 	addComment,
 	isReplyField = false,
 }) => (
-	<HStack className="noprint" width="100%" alignItems="center">
-		<Box flex={11}>
-			{!isReplyField && comment?.blocks?.length > 0 && (
-				<Text fontSize="sm" color="gray">
-					Your Comment will be linked to the blocks you clicked on.
-					<br />
-					Click On Those Block Comment Buttons again to remove them from this
-					comment.
-					<br />
-				</Text>
-			)}
-			<CommentTextField
-				userOptions={editorUsers || []}
-				onChange={handleCommentTextChange}
-				isReplyField={isReplyField}
-			/>
-		</Box>
-		<Box flex={1} textAlign="right">
-			<IconButton colorScheme="blue" onClick={addComment}>
-				<BiSend size="1.5rem" />
-			</IconButton>
-		</Box>
-	</HStack>
+	<div className="noprint">
+		<HStack width="100%" alignItems="center">
+			<Box flex={11}>
+				{!isReplyField && comment?.blocks?.length > 0 && (
+					<Text fontSize="sm" color="gray">
+						Your Comment will be linked to the blocks you clicked on.
+						<br />
+						Click On Those Block Comment Buttons again to remove them from this
+						comment.
+						<br />
+					</Text>
+				)}
+				<CommentTextField
+					userOptions={editorUsers || []}
+					onChange={handleCommentTextChange}
+					isReplyField={isReplyField}
+					commentText={comment.text}
+				/>
+			</Box>
+			<Box flex={1} textAlign="right">
+				<IconButton colorScheme="blue" onClick={addComment}>
+					<BiSend size="1.5rem" />
+				</IconButton>
+			</Box>
+		</HStack>
+	</div>
 );
 
 export default CommentInputBlock;
--- frontend/src/components/Document/CommentTextField.jsx
+++ frontend/src/components/Document/CommentTextField.jsx
@@ -1,41 +1,32 @@
 import styled from "@emotion/styled";
-import Mentions from "rc-mentions";
-const { Option } = Mentions;
 
-const CommentTextFieldWrapper = styled(Mentions)`
-	textarea {
-		width: 100%;
-		border: 0.075rem solid var(--bordergrey);
-		border-radius: calc(0.25 * var(--standard-spacing));
-		padding: ${({ $isReplyField }) =>
-			$isReplyField
-				? "calc(0.5 * var(--standard-spacing))"
-				: "var(--standard-spacing)"};
-		min-height: ${({ $isReplyField }) => ($isReplyField ? "1.5vh" : "15vh")};
-		background: var(--white);
-		color: var(--black);
-		margin-top: calc(0.25 * var(--standard-spacing));
-	}
+const CommentTextArea = styled.textarea`
+	width: 100%;
+	border: 0.075rem solid var(--bordergrey);
+	border-radius: calc(0.25 * var(--standard-spacing));
+	padding: ${({ $isReplyField }) =>
+		$isReplyField
+			? "calc(0.5 * var(--standard-spacing))"
+			: "var(--standard-spacing)"};
+	min-height: ${({ $isReplyField }) => ($isReplyField ? "1.5vh" : "15vh")};
+	background: var(--white);
+	color: var(--black);
+	margin-top: calc(0.25 * var(--standard-spacing));
 `;
 
 const CommentTextField = ({
 	isReplyField = false,
 	onChange = () => null,
-	userOptions = [],
+	commentText = "",
 }) => {
 	return (
-		<CommentTextFieldWrapper
+		<CommentTextArea
 			placeholder={isReplyField ? "Write Your Reply" : "Write your comment"}
+			value={commentText}
 			onChange={onChange}
 			transitionName="motion"
 			$isReplyField={isReplyField}
-		>
-			{userOptions?.length
-				? userOptions?.map((user) => (
-						<Option value={user.id}>{user.displayName}</Option>
-				  ))
-				: "No Users Available"}
-		</CommentTextFieldWrapper>
+		/>
 	);
 };
 
