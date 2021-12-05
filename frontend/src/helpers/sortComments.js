ba30a4359c72d843b0732d81c75658a1c04871d9
Devesh Kumar <devesh2027@gmail.com>
2021-12-05 17:09:01 +0530

    Reversed sorting order of comments and replies to traditional

--- frontend/src/helpers/sortComments.js
+++ frontend/src/helpers/sortComments.js
@@ -2,8 +2,8 @@ const sortComments = (comments = []) => {
 	let sortedComments = [...comments];
 	sortedComments.sort(
 		(comment1, comment2) =>
-			comment2?.createdAt?.toDate?.()?.getTime() -
-			comment1?.createdAt?.toDate?.()?.getTime()
+			comment1?.createdAt?.toDate?.()?.getTime() -
+			comment2?.createdAt?.toDate?.()?.getTime()
 	);
 	return sortedComments;
 };
